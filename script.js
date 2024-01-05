83614a093c2f9f09d0e19d13151fbec127c67f66
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-01-05 04:11:44 +0000

    tracking score

--- script.js
+++ script.js
@@ -3,56 +3,31 @@
 // Rock > Scissors
 // Scissors > Paper
 
-function play(userChoice, botChoice) {
+function playRound(userChoice, botChoice) {
+
+    console.log("You chose", userChoice);
+    console.log("The Bot chose", botChoice);
 
     if (botChoice === "paper" && userChoice === "rock") {
         console.log("You lose! Paper beats Rock!");
+        return "lost"
     }
     else if (botChoice === "rock" && userChoice === "scissors") {
         console.log("You lose! Rock beats Scissors!");
+        return "lost"
     }
     else if (botChoice === "scissors" && userChoice === "paper") {
         console.log("You lose! Scissors beats Paper!");
+        return "lost"
     }
 
     else if (botChoice === userChoice) {
         console.log("It's a draw!");
-        return "draw";
-    }
 
-    else {
-        console.log(`You win! ${userChoice} beats ${botChoice}!`);
-    }
-}
+        // another round
+        //var userChoice = prompt("Rock, Paper or Scissors?");
+        var userChoice = "scissors";
 
-// get bot choice
-function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"];
-    var random = Math.floor(Math.random() * options.length);
-    var botChoice = options[random];
-    console.log("The Bot chose", botChoice);
-    return botChoice
-}
-
-
-// Get user's choice
-
-var userChoice = prompt("Rock, Paper or Scissors?");
-
-if (userChoice === null) {
-    console.log("You don't want to play :(");
-}
-
-else {
-    var userChoice = userChoice.toLowerCase();
-    console.log("You chose", userChoice);
-
-    var botChoice = getComputerChoice();
-
-    var result = play(userChoice, botChoice);
-
-    if (result === "draw") {
-        var userChoice = prompt("Rock, Paper or Scissors?");
 
         if (userChoice === null) {
             console.log("I guess you lose!");
@@ -61,9 +36,58 @@ else {
         else {
             console.log("You chose", userChoice);
             var botChoice = getComputerChoice();
-            play(userChoice, botChoice);
+            playRound(userChoice, botChoice);
         }
-
     }
 
+    else {
+        console.log(`You win! ${userChoice} beats ${botChoice}!`);
+        return "won"
+    }
+}
+
+// get bot choice
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    var random = Math.floor(Math.random() * options.length);
+    var botChoice = options[random];
+    return botChoice;
+}
+
+// Game
+
+function game() {
+    var i = 0;
+    var userScore = 0;
+    var botScore = 0;
+
+    while (i < 5) {
+        var botChoice = getComputerChoice();
+        var result = playRound(userChoice, botChoice);
+        if (result === "won") {
+            userScore = userScore + 1;
+        }
+
+        else if (result === "lost") {
+            botScore = botScore + 1;
+        }
+        console.log(`Your score is ${userScore}`);
+        console.log(`The Bot's score is ${botScore}`);
+        i++;
+    }
+}
+
+// Get user's choice
+
+//var userChoice = prompt("Rock, Paper or Scissors?");
+var userChoice = "scissors";
+
+if (userChoice === null) {
+    console.log("You don't want to play :(");
+}
+
+else {
+    // Play the game
+    var userChoice = userChoice.toLowerCase();
+    game();
 }

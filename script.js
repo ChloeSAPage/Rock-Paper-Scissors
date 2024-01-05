c3ed8e62e0a9fc47024d08af8977591192e0d934
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-01-05 03:07:06 +0000

    added logic of game

--- script.js
+++ script.js
@@ -3,17 +3,40 @@
 var userChoice = prompt("Rock, Paper or Scissors?");
 
 if (userChoice === null){
-    console.log("You don't want to play :(")
+    console.log("You don't want to play :(");
 }
 
 else{
-    var userChoice = userChoice.toLowerCase()
-    console.log("User = ", userChoice)
+    var userChoice = userChoice.toLowerCase();
+    console.log("User = ", userChoice);
 }
 
 // get bot choice
 const options = ["rock", "paper", "scissors"];
 var random = Math.floor(Math.random() * options.length);
-var botChoice = options[random]
+var botChoice = options[random];
 
-console.log("Bot = ", botChoice)
+console.log("Bot = ", botChoice);
+
+// Determine who won
+// Paper > Rock
+// Rock > Scissors
+// Scissors > Paper
+
+if (botChoice === "paper" && userChoice === "rock"){
+    console.log("You lose!");
+}
+else if (botChoice === "rock" && userChoice === "scissors"){
+    console.log("You lose!");
+}
+else if (botChoice === "scissors" && userChoice === "paper"){
+    console.log("You lose!");
+}
+
+else if (botChoice === userChoice){
+    console.log("It's a draw!")
+}
+
+else{
+    console.log("You win!");
+}

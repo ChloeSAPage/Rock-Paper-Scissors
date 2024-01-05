d5d411a180381afc39fdcd77ea5381e156c9b77a
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-01-05 03:09:19 +0000

    game only runs when user chooses

--- script.js
+++ script.js
@@ -2,41 +2,41 @@
 
 var userChoice = prompt("Rock, Paper or Scissors?");
 
-if (userChoice === null){
+if (userChoice === null) {
     console.log("You don't want to play :(");
 }
 
-else{
+else {
     var userChoice = userChoice.toLowerCase();
-    console.log("User = ", userChoice);
-}
+    console.log("You chose", userChoice);
 
-// get bot choice
-const options = ["rock", "paper", "scissors"];
-var random = Math.floor(Math.random() * options.length);
-var botChoice = options[random];
+    // get bot choice
+    const options = ["rock", "paper", "scissors"];
+    var random = Math.floor(Math.random() * options.length);
+    var botChoice = options[random];
 
-console.log("Bot = ", botChoice);
+    console.log("The Bot chose", botChoice);
 
-// Determine who won
-// Paper > Rock
-// Rock > Scissors
-// Scissors > Paper
+    // Determine who won
+    // Paper > Rock
+    // Rock > Scissors
+    // Scissors > Paper
 
-if (botChoice === "paper" && userChoice === "rock"){
-    console.log("You lose!");
-}
-else if (botChoice === "rock" && userChoice === "scissors"){
-    console.log("You lose!");
-}
-else if (botChoice === "scissors" && userChoice === "paper"){
-    console.log("You lose!");
-}
+    if (botChoice === "paper" && userChoice === "rock") {
+        console.log("You lose!");
+    }
+    else if (botChoice === "rock" && userChoice === "scissors") {
+        console.log("You lose!");
+    }
+    else if (botChoice === "scissors" && userChoice === "paper") {
+        console.log("You lose!");
+    }
 
-else if (botChoice === userChoice){
-    console.log("It's a draw!")
-}
+    else if (botChoice === userChoice) {
+        console.log("It's a draw!")
+    }
 
-else{
-    console.log("You win!");
+    else {
+        console.log("You win!");
+    }
 }
